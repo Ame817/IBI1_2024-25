087d84ff9af6ec6da0be557b9dc3b3061c33679b
ZOU Ruizhi <ruizhi.24@intl.zju.edu.cn>
2025-03-19 15:15:34 +0800

    edit

--- Practical5/language.py
+++ Practical5/language.py
@@ -1,15 +1,16 @@
 options={'JavaScript':62.3,'HTML':52.9,'Python':51,'SQL':51,'TypeScript':38.5} #create the dictionary
-print(optionspy)
-language='HTML' #language is the modified variable
+print(options)
+language=input("Please input a language:") #language is the modified variable here
+#print the results
 if language in options:
-    print(language, "'s percentage of developers who use this language is " ,options[language])
+    print("The percentage of developers who use",language,"is" ,options[language],"%")
 else:
     print("Sorry, data of this language is not found")
 import matplotlib.pyplot as plt
 import numpy as np
 
-x=np.array(["JavaScript","HTML","Python","SQL","TypeScript"])
-y=np.array([62.3,52.9,51,51,38.5])
+#generate the pie chart
+x=np.array(["JavaScript","HTML","Python","SQL","TypeScript"]) 
+y=np.array([62.3,52.9,51,51,38.5]) 
 plt.bar(x,y,color="#2B6868") #create the bar plot with the color #2B6868
 plt.show() #show the bar plot
-
--- Practical5/population_size.py
+++ Practical5/population_size.py
@@ -1,45 +1,66 @@
-#store the data in lists
-uk_countries=[57.11,3.13,1.91,5.45]
-china_provinces=[65.77,41.88,45.28,61.27,85.15]
-
-print('The population in 2022 of each of the component countries in the UK:',uk_countries)
-print('The population in 2022 of each of the provinces in China which corder Zhejiang province',china_provinces)
-
-#print the lists
-labels1=['England','Wales','Northern_Ireland','Scotland']
-labels2=['Zhejiang','Fujian','Jiangxi','Anhui','Jiangsu']
-
-#print the lists()
-print('UK countries populations:')
-for labels1, uk_countries in zip(labels1, uk_countries):
-    print(f"{labels1}: {uk_countries}", 'millions')
-
-print('China provinces populations:')
-for labels2, china_provinces in zip(labels2, china_provinces):
-    print(f"{labels2}: {china_provinces}",'millions')
-
 import matplotlib.pyplot as plt
 import numpy as np 
 
-#the data, labels and colors for the first chart of the UK countries
+#------store the data in lists, then sort the data
+#*the data and labels should be sorted together
+
+#-store the data in lists
 uk_countries=[57.11,3.13,1.91,5.45]
-x=np.array(uk_countries)
-colors1=['#F18289','#FCD19C','#BED2C7','#CCDFE6']
+china_provinces=[65.77,41.88,45.28,61.27,85.15]
 labels1=['England','Wales','Northern_Ireland','Scotland']
-#generate and show the first pie chart
+labels2=['Zhejiang','Fujian','Jiangxi','Anhui','Jiangsu']
+
+#-combine the data and the labels together, then sort the combined lists; after that, separate them
+#combine the two lists together
+combineduk=list(zip(uk_countries,labels1))
+combinedchina=list(zip(china_provinces,labels2))
+#sort the combined lists
+combineduk.sort()
+combinedchina.sort()
+#separate the sorted lists
+uk_countries[:], labels1[:] = zip(*combineduk)
+china_provinces[:], labels2[:] = zip(*combinedchina)
+
+
+#print the sorted lists of populations in UK countries and China provinces
+print('The population in 2022 of each of the component countries in the UK:',uk_countries)
+print('The population in 2022 of each of the provinces in China which corder Zhejiang province',china_provinces)
+
+#--------generate the pie chart
+#put the two charts into one window
+#The charts should include labels and percentage(in the pie chart) for each segment.
+
+#the data, labels and colors for the first chart of the UK countries
+x=np.array(uk_countries)
+colors1=['#F18289','#FCD19C','#BED2C7','#CCDFE6'] 
+#generatethe first pie chart
 plt.figure(1)
-plt.pie(x,labels=labels1,colors=colors1)
+plt.pie(x,labels=labels1,colors=colors1,autopct='%.1f%%') #"autopct='%.1f%%':show the percentage in the pie chart
 plt.title('The population in 2022 of each of the component countries in the UK (millions)')
 
 #the data, labels and colors for the first chart of the China provinces
-china_provinces=[65.77,41.88,45.28,61.27,85.15]
 y=np.array(china_provinces)
-labels2=['Zhejiang','Fujian','Jiangxi','Anhui','Jiangsu']
 colors2=['#FFE9A2','#E9D0BC','#BAC5C6','#9DA5C1','#676891']
-#generate and show the first pie chart
+#generate the second pie chart
 plt.figure(2)
-plt.pie(y,labels=labels2,colors=colors2)
+plt.pie(y,labels=labels2,colors=colors2,autopct='%.1f%%')
 plt.title('The population in 2022 of each of the provinces in China which corder Zhejiang province (millions)')
-#show the pie chart
+
+#---------print the percentage with their labels together, directly showing the informations to the user
+#desired output format: someplace: xx millions
+
+print('\n UK countries populations:')
+for labels1, uk_countries in zip(labels1, uk_countries):
+    print(f"{labels1}: {uk_countries}", 'millions')
+
+print('\n China provinces populations:')
+for labels2, china_provinces in zip(labels2, china_provinces):
+    print(f"{labels2}: {china_provinces}",'millions')
+
+#--warnings:this section should only put here
+#if it is moved below "plt.show()",the program will hang, and the information won't be printed.
+#if it is moved before pie charts generating codes, "uk_countries" and "china_provinces" cannot be successfully identified
+
+#--------show the pie chart
 plt.show()
 
