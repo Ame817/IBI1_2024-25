7d80c121b0dc27631a1930acf193247af6bc5281
unknown <ruizhi.24@intl.zju.edu.cn>
2025-04-22 16:03:17 +0100

    Practical4

---
+++ Practical4/variable.py
@@ -0,0 +1,20 @@
+a=15
+b=75
+c=a+b
+d=90
+e=5
+f=d+e
+print("bus:",c,"minutes")
+print("walk:",f,"minutes")
+if c<f:
+    print("taking bus is faster")
+else:
+    print("driving is faster")
+#c<f
+#taking bus is faster
+
+X=True
+Y=False
+W=X and Y
+print("X =",X,"Y =",Y,"W =",W)
+#"X= True Y= False W= False"
