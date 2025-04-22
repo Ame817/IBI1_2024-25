6cfe8419e9bd2820ccb65b9e1d3e775f551898b9
unknown <ruizhi.24@intl.zju.edu.cn>
2025-04-22 16:01:25 +0100

    modified

--- Practical4_tempo/triangle.py
+++
@@ -1,7 +0,0 @@
-i=0 #start with i=0
-#start a loop to count the triangle numbers
-for i in range(0,10):
-    i += 1 #i=i+1 for each loop
-    triangle_number=sum(range(1,i+1)) #count triangle number for each i by summing up 1 to i
-    print(triangle_number) #print the results
-
